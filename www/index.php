cd3fd5756ece2b4e994e2b91d5427d7c73439805
walker <walker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-27 18:54:56 +0000

    Changed site title and added a list of the two most important features of multitable.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multitable@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -23,29 +23,25 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 
 <body>
 
-<!-- R-Forge Logo -->
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tr><td>
-<a href="http://r-forge.r-project.org/"><img src="http://<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
-</table>
+
 
 
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
-
+<h2>General multiple-table data management</h2>
+	
+<p>Our objective is to develop classes of objects that (1) make handling multiple-table data sets easier and (2) seamlessly integrate with existing R plotting and model-fitting functions; our philosophy is to keep data management and data analysis separate.
 <!-- end of project description -->
 
 <p> Many ecologists and other scientists face a particular kind of data management issue, arising from data sets that cannot be expressed as a single table, matrix, or array.  The prototypical example of this issue, called the fourth-corner problem (Legendre et al. 1997), comes from trait-based studies of ecological communities in which the ecologist must manage three tables:  a sites-by-species table of community data, a sites-by-environmental variables table, and a species-by-traits table.  Such a data set cannot be expressed in a single table (e.g. an object of class data frame or array) without either (1) summarizing or (2) repeating chunks of data; hence, the problem is inherently multi-tabular.  
 
-The multitable project involves the development of R tools supporting the analysis of such multiple-table data.  The specific objective of the multitable project is to develop new classes of objects that make handling multiple-table data sets easier, and seamlessly integrate with existing R model fitting functions.  The framework being developed is based on a separation of data management and data analysis.  Once multiple-table data are organized into a special multiple-table R object (i.e. the data management step) they can be passed to plotting and model fitting functions (i.e. the data analysis step), just as we would pass data frames to such functions in R.  Having separated analysis from data management, analyzing multiple tables becomes conceptually equivalent to analyzing single tables.</p>
+The multitable project involves the development of R tools supporting the analysis of such multiple-table data.  The specific objective of the multitable project is to develop new classes of objects that make handling multiple-table data sets easier, and seamlessly integrate with existing R model fitting functions.  The framework being developed is based on a separation of data management and data analysis.  Once multiple-table data are organized into a special multiple-table R object (i.e. the data management step) they can be passed to plotting and model fitting functions (i.e. the data analysis step), just as we would pass data frames to such functions in R.  Having separated analysis from data management, analyzing multiple tables becomes conceptually equivalent to analyzing single tables.
+
+Major features: (1) subscripting of multiple tables simultanenously, (2) coercion of multiple tables into a single data frame for use in standard R model fitting and plotting functions.
+	
+</p>
+
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
