cc0f03063f236cb22f604d5b47f756d4beed4c51
walker <walker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-03 16:40:03 +0000

    Fixed minor S3 generic inconsistency problem
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multitable@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/multitable/R/summary.data.list.R
+++ pkg/multitable/R/summary.data.list.R
@@ -2,7 +2,7 @@ str.data.list <- function(object, give.attr = FALSE, ...){
 	str(as.list(object), give.attr = give.attr, ...)
 }
 
-summary.data.list <- function(x){
+summary.data.list <- function(object, ...){
 	x <- unclass(x)
 	out <- list()
 	out$dims <- sapply(x,attr,"subsetdim")
