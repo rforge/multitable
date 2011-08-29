1d289aa17ca749017a0943c6182df18f15ade676
walker <walker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-29 17:56:47 +0000

    Added a dlcast function for taking a list of data molten (i.e. long) frames and making a casted data list.  Also fixed some bugs with how dimnames are handled by as.data.list.  Also moved the helper functions for as.data.list to the as.data.list file.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multitable@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/multitable/R/dlcast.R
@@ -0,0 +1,42 @@
+dlcast <- function(x,dnames){
+	xnames <- lapply(x,names)
+	dims <- lapply(xnames,intersect,dnames)
+	vars <- setdiff(unlist(xnames),dnames)
+	vars <- lapply(xnames,intersect,vars)
+	out <- list()
+	for(i in seq_along(x)){
+		if(is.null(vars[[i]])) next
+		dim.namesi <- lapply(dims[[i]],get.dim.names,x)
+		full <- mouter(dim.namesi,FUN=paste,sep=".")
+		dfull <- dim(full)
+		if(is.null(dfull)) dfull <- length(full)
+		obs <- do.call(paste,c(x[[i]][dims[[i]]],sep="."))
+		mtchs <- match(obs,full)
+		out[[i]] <- list()
+		for(j in seq_along(vars[[i]]))
+			out[[i]][[j]] <- make.arrays(vars[[i]][j],x[[i]],mtchs,dfull,dim.namesi)
+		names(out[[i]]) <- vars[[i]]
+	}
+	out <- as.data.list(out,match.dnames=dims)
+	return(out)
+}
+
+mouter <- function(x,...){
+	spouter <- function(x,y) outer(x,y,...)
+	Reduce("spouter",x)
+}
+
+get.dim.names <- function(dimsi,x){
+	dim.name.list <- lapply(lapply(x,'[[',dimsi),as.character)
+	dim.names <- unique(unlist(dim.name.list))
+	dim.names
+}
+
+make.arrays <- function(vr,xi,mtchs,dfull,nms){
+	a <- array(NA,dim=dfull)
+	xv <- xi[[vr]]
+	a[mtchs] <- if(is.factor(xv)) as.character(xv) else xv
+	if(length(dfull)==1) names(a) <- nms[[1]]
+	else dimnames(a) <- nms
+	return(a)
+}
