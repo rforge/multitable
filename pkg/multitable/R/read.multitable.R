c0d9e8fbab4a0f3e76780bc7ca4cfefa9156bf30
walker <walker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-31 13:34:35 +0000

    Some new features for reading multiple text files.   Sorry for the unhelpful message steve of the future.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multitable@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/multitable/R/read.multitable.R
+++ pkg/multitable/R/read.multitable.R
@@ -11,12 +11,16 @@ read.multicsv <- function(files, dnames, tnames, fill = rep(NA,length(files)),..
 read.multidelim <- function(files, dnames, tnames, fill = rep(NA,length(files)),...)
 	read.multitable(files, dnames, tnames, fill, sep="\t", ...)
 
-read.fourthcorner <- function(Y, X, Z, dnames=c("sites","species"), Yname="abundance",...){
-	Y <- as.matrix(read.table(Y, ...))
-	X <- read.table(X, ...)
-	Z <- read.table(Z, ...)
+read.fourthcorner <- function(community, environment, traits, dnames=c("sites","species"), community.name="abundance",...){
+	community <- as.matrix(read.table(community, ...))
+	environment <- read.table(environment, ...)
+	traits <- read.table(traits, ...)
 	match.dnames <- list(dnames,dnames[1],dnames[2])
-	dl <- data.list(Y,X,Z,match.dnames=match.dnames)
-	names(dl)[1] <- Yname
+	dl <- data.list(community,environment,traits,match.dnames=match.dnames)
+	names(dl)[1] <- community.name
 	return(dl)
+}
+
+multifile.choose <- function(n){
+	sapply(rep(FALSE,n),file.choose)
 }
