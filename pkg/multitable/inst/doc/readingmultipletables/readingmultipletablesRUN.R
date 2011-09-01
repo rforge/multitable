637b243cb44cf8bdc43c58f7c24fde98a3fb7dab
walker <walker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-01 21:58:57 +0000

    Updated the main vignette, especially the figures (now done in tikz).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multitable@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/multitable/inst/doc/readingmultipletables/readingmultipletablesRUN.R
+++ pkg/multitable/inst/doc/readingmultipletables/readingmultipletablesRUN.R
@@ -1,2 +1,2 @@
 setwd("/users/stevenwalker/documents/multitable/multitable/pkg/multitable/inst/doc/")
-Sweave("/users/stevenwalker/documents/multitable/multitable/pkg/multitable/inst/doc/readingmultipletables.Rnw")
+Sweave("/users/stevenwalker/documents/multitable/multitable/pkg/multitable/inst/doc/multitable.Rnw")
