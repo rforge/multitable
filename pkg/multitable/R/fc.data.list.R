f2cfe043c94a2e02e35d3dee4a3f239a333a6a11
walker <walker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-26 02:19:35 +0000

    fixed up fc.data.list file to hopefully work with the 'new' multitable.  updated the as.data.list help file.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multitable@70 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/multitable/R/fc.data.list.R
+++ pkg/multitable/R/fc.data.list.R
@@ -1,7 +1,7 @@
 fc.data.list <-
 function(Y,X,Z,dnames=c("sites","species"),respname="community"){
-	dl <- list(Y,as.data.frame(X),as.data.frame(Z))
+	dl <- list(as.matrix(Y),as.data.frame(X),as.data.frame(Z))
 	names(dl) <- c(respname,"","")
-	data.list(dl,list(dnames,dnames[1],dnames[2]))
+	as.data.list(dl,match.dimids = list(dnames,dnames[1],dnames[2]))
 }
 
