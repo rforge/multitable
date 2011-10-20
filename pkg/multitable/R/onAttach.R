14efd69db5fd3d3d600c6c09d7e75e20d942792b
walker <walker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-10-20 15:58:49 +0000

    Actually added the banner file, via the onAttach.R file.  Moved the test.R file into a test directory.  Added a NEWS file.  Ready to update on CRAN.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/multitable@80 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/multitable/R/onAttach.R
@@ -0,0 +1,6 @@
+".onAttach"=function(libname, pkgname) {
+    if (interactive()) {
+        packageStartupMessage('Welcome to multitable ',as.character(packageVersion("multitable")),'\nType vignette("multitable") or help("multitable") to get started')
+    }
+}
+
